8cdc3d239e9d94f21e21a27c0f86ea3675c081ea
Tyler <tylerdh12@gmail.com>
2019-03-15 01:43:25 +0000

    Final Completed Project
    This is my final completed project

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Tyler Harper
 ******************************************/
 
-/* Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-Start of quotes
+/* Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing*/
+/*Start of quotes
 ------------------------------------------------------------------------------*/
+
 var quotes = [
     {
       /*individual qote will need to be pulled using a random number generator then use that value to select the quote at random */
-        quote : 'Although the world is full of suffering, it is full also of the overcoming of it.',
+        quote : 'Although the world is full of suffering, it is full also of the overcoming of it. My optimism, then, does not rest on the absence of evil, but on a glad belief in the preponderance of good and a willing effort always to cooperate with the good, that it may prevail.',
         source : 'Helen Keller',
         citation : 'US blind & deaf educator',
-        year :'1903'
+        year : '1903',
+        category : ['Optimism', 'Hope']
     },
     {
         quote : 'Luck is the residue of design.',
@@ -25,31 +27,35 @@ var quotes = [
         quote : 'My theory of evolution is that Darwin was adopted.',
         source : 'Steven Wright',
         citation : 'US comedian and actor',
-        year : '1955'
+        year : '1955',
+        category : ['Funny', 'Theory']
     },
     {
         quote : 'It\'s always been about the experience of life and of not being passive. If something so excites my curiosty, I want to go there, be a part of whatever it is that\'s either pushing me or pulling me toward it. That desire for experience has made me very rich in life experience.',
         source : 'Tish Grier',
         citation : 'love and hope and sex and dreams',
-        year : '2006'
+        year : '2006',
+        category : ['Fulfilment', 'Life', 'Passion']
     },
     {
         quote : 'The greatest obstacle to discovery is not ignorance - it is the illusion of knowledge.',
         source : 'Daniel J. Boorstin',
         citation : 'US historian',
-        year : '1914'
+        year : '1914',
+        category : ['Wisdom']
     },
     {
         quote : 'We should conduct ourselves not as if we ought to live for the body, but as if we could not live without it.',
         source : 'Seneca',
         citation : 'Roman dramatist, philosopher, & politician',
-        year : '5 BC - 65 AD'
+        year : '5 BC - 65 AD',
+        category : ['Health']
     },
     {
         quote : 'Two years from now, spam will be solved.',
         source : 'Bill Gates',
-        citation : 'Founder of Microsoft Corporation',
-        year : '2004'
+        year : '2004',
+        category : ['Prediction']
     }
 ];
 /*------------------------------------------------------------------------------
@@ -58,99 +64,80 @@ END of quotes */
 //Used to test full list of quotes in console
 //console.log(quotes.year);
 
-
-
-/* The function should look like this ----  Below
-function getRandomQuote() {
-generate a random number between 0 and the last index in the array parameter
-use the random number and box notation to grab a random item from the quotes array
-return the random item
-}
-*/
-
 //Start of random quote function
 function getRandomQuote() {
-  //create a variable to store the random number generated from 0 to the length of the quotes
+  //create a variable to store the random number generated from 0 to the length of the quotes array of objects
   var randomNum = Math.floor( Math.random() * quotes.length );
-  //grabbing a random quote using the randomNum variable
+  //grabbing a random quote using the randomNum variable and returning that value
   return(quotes[randomNum]);
   //just to see if it is creating a random number use the log below
   //console.log(randomNum);
 }
 
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote varable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
+//This function creates a random color
+function getRandomColor() {
+  //we first have to give the function charictors to choose to randomize so we can use a hex '#' color code
+   var availableLettersNumbers = '0123456789ABCDEF';
+   //now we need a variable to start with for the color place holder is # so we dont have to concat later
+   var newColor = '#';
+   //got to create a standard for loop that goes thro the loop 6 times to create a wider range of colors
+   for (var i = 0; i < 6; i++) {
+     //now the random string gets added to our original # for the hash color using math.floor *16 because it will go thru all the available combinations of our availableLettersNumbers variable
+     newColor += availableLettersNumbers[Math.floor(Math.random() * 16)];
+   }
+   //we now need to return that new color value to where it can be used externally
+   return newColor;
+ }
 
-/*  Funstion should look like this ----  Below
+/*start of print function
+------------------------------------------------------------------------------*/
 function printQuote() {
-// create a variable that calls the getRandomQuote() function
-// create a variable that initiates your HTML string
-// using the template in the project instructions, add the two default quote
-properties, quote and source
-// if there is a quote.citation property, add it the string
-// if there is a quote.year property, add it the string
-// close the string with the necessary closing HTML tags
-// set the innnerHTML of the .quote-box to the complete HTML string
-}
-*/
 
+  //this changes the color for the background and needs to be inside this funcrion to call it every time it asks for a new quote
+  var randomColor = getRandomColor();
+  //this actually alters the body style to changes the body background property of the index.HTML file
+  document.body.style.background = randomColor;
 
-//start of print function
-function printQuote() {
+  //creates a variable for the getRandomQuote function to be called and be stored
   var ranQuote = getRandomQuote();
+
   /*to test the random quote output using the printQuote function use logs below
-  ----------------------------------------------------------------------------
-  */
+  ----------------------------------------------------------------------------*/
   //console.log(ranQuote.quote);
   //console.log(ranQuote.source);
   //console.log(ranQuote.citation);
   //console.log(ranQuote.year);
 
-
-  //quote string html output
-  var quoteOutput = '<div class="quote-box"><p class=\"quote\">' + ranQuote.quote + '</p>';
+  /* Quote string html output starts here
+  ----------------------------------------------------------------------------*/
+  //This line creates the quote string in HTML form
+  var quoteOutput = '<p class="quote">' + ranQuote.quote + '</p>';
+    //This line adds on or concatenates the prior output to the quotes source or author
     quoteOutput += ' <p class="source">' + ranQuote.source;
-
-    if (ranQuote.citation != null ){
+    /*before adding the citation we need to make sure the object containes the
+    information. I used an if statement that checks to see if the objects value
+    is empty. Then adds the citation in HTML format if it is available. */
+    if (ranQuote.citation != undefined ){
       quoteOutput += '<span class="citation"> '+ ranQuote.citation + '</span>';
     }
-    if (ranQuote.year != null ){
+    //this if statement does the same for the year string to be added to the quote output
+    if (ranQuote.year != undefined ){
       quoteOutput += '<span class="year"> '+ ranQuote.year + '</span>';
     }
-
+    //this if statement does the same for the category string to be added to the quote output but runs a for look to seperate each item in the category so it has its own box.
+    if (ranQuote.category != undefined ){
+      for (i = 0; i < ranQuote.category.length; i++){
+        quoteOutput += '<p class="category"> '+ ranQuote.category[i] + '</p>';
+      }
+    }
+    //this adds the closing paragraph tag and in this case completes the concatenation that needs to be done for our output
     quoteOutput += '</p>';
-    // quote source output string
-    //adding the if statement to check to see if citation is null then do nothing but if NOT empety add string to quoteOutput
+    //this line replaced the content in the quote-box div with the output string created above
     document.getElementById('quote-box').innerHTML = quoteOutput;
 }
 
-  /*  Make the HTML print out this way
-  <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-  <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
-  /*
+//This line allows you to set the delay for the quotes and background
+setInterval(printQuote, 6000);
 
-}
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
+//this line listens for an action ' click ' on the button to reload out print quote function and reload a new random quote
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
